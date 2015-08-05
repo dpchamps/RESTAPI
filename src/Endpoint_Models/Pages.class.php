d9c0905780aed03275b4d47b8f2b1f6a79091275
gasolinewaltz <gasolinewaltz@gmail.com>
2015-08-05 14:15:48 -0700

    merch function abstraction

--- src/Endpoint_Models/Pages.class.php
+++ src/Endpoint_Models/Pages.class.php
@@ -14,6 +14,10 @@ class Pages {
     private $args;
     private $method;
     private $item;
+
+    /*
+     * case 'menu'
+     */
     private function menu_item_edit(){
         $id = $this->util->check($this->item['id']);
 
@@ -68,18 +72,6 @@ class Pages {
         //return $this->lists->order_menu_array($raw_array);
 
     }
-
-
-    public function __construct($args, $method){
-        $this->args = $args;
-        $this->method = $method;
-
-        $this->db = Database::get_instance();
-        $this->lists = new List_functions();
-        $this->sql = new SQL_Statements();
-        $this->util = new Utilities($this->method);
-    }
-
     public function get_menu($item)
     {
         array_shift($this->args);
@@ -96,28 +88,63 @@ class Pages {
         return $this->menu();
 
     }
-
-    public function get_merch($item)
-    {
-        $response = NULL;
-        //select all merch titles from db
-        $types = Database::get_instance()->select('title', 'merch_items', Array());
-        $types = Database::get_instance()->fetch_all($types);
-
-        if ($this->util->check($item)) {
-            $item_id = $this->db->select_single_item('id', 'merch_items', Array('title' => $item));
+    /*
+    * case 'merch'
+    */
+    private function merch_edit(){
+        $id = $this->util->check($item['id']);
+        if(!$id){
+            throw new Exception(400);
+        }
+    }
+    private function merch_item(){
+        if($this->method === 'GET') {
+            $item_id = $this->db->select_single_item('id', 'merch_items', Array('title' => $this->item));
             if ($item_id) {
                 $text_query = $this->sql->get('merch', $item_id, 'text');
                 $image_query = $this->sql->get('merch', $item_id, 'image');
                 $response = $this->db->fetch_all_query($text_query);
                 $response['images'] = $this->db->fetch_all_query($image_query);
-                $response = $this->lists->build_merch($response);
+                return $this->lists->build_merch($response);
+            }else{
+                throw new Exception(404);
             }
+        }else{
+            return $this->merch_edit();
+        }
+    }
+    public function get_merch($item)
+    {
+        $this->item = $item;
+        $response = NULL;
+        //select all merch titles from db
+        $types = Database::get_instance()->select('title', 'merch_items', Array());
+        $types = Database::get_instance()->fetch_all($types);
+
+        if ($this->item) {
+            $this->util->allowed_methods('GET PUT POST PATCH');
+            return $this->merch_item();
         } else {
+            $this->util->allowed_methods('GET');
             $response = $types;
         }
         return $response;
     }
+
+
+    public function __construct($args, $method){
+        $this->args = $args;
+        $this->method = $method;
+
+        $this->db = Database::get_instance();
+        $this->lists = new List_functions();
+        $this->sql = new SQL_Statements();
+        $this->util = new Utilities($this->method);
+    }
+
+
+
+
     public function get_press($item)
     {
         $response = NULL;
